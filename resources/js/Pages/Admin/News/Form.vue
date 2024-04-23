b2360000619ee8406288eeec35e29354f5d1fe6a
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2024-04-23 13:19:36 +0700

    news crud

---
+++ resources/js/Pages/Admin/News/Form.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+const props = defineProps({
+    news: {
+        type: Object,
+    },
+});
+
+const form = useForm({
+  title: props.news?.title ?? '',
+  slug: props.news?.slug ?? '',
+  content: props.news?.content ?? '',
+  image: null, // TODO: handle image
+});
+
+const submit = () => {
+  if (props.news) {
+    form.put(route('news.update', props.news.id), {
+      onFinish: () => form.reset(),
+    });
+  } else {
+    form.post(route('news.store'), {
+      onFinish: () => form.reset(),
+    });
+  }
+};
+
+</script>
+
+<template>
+
+  <Head title="News" />
+
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        News
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-between">
+          <Link :href="route('news.index')"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Back
+          </Link>
+        </div>
+
+        <form @submit.prevent="submit" class="m-4">
+          <div class="grid grid-cols-1 gap-6">
+            <div>
+              <label for="title" class="block text-sm font-medium text-white">Title</label>
+              <input type="text" name="title" id="title" v-model="form.title" required
+                class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+            </div>
+
+            <div>
+              <label for="slug" class="block text-sm font-medium text-white">Slug</label>
+              <input type="text" name="slug" id="slug" v-model="form.slug" required
+                class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+            </div>
+
+            <div>
+              <label for="content" class="block text-sm font-medium text-white">Content</label>
+              <textarea name="content" id="content" v-model="form.content" required
+                class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"></textarea>
+            </div>
+
+            <div>
+              <label for="image" class="block text-sm font-medium text-white">Image</label>
+              <input type="file" name="image" id="image" @change="form.image = $event.target.files[0]"
+                class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+            </div>
+
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+              {{
+                news ? 'Update News' : 'Create News'
+              }}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </AuthenticatedLayout>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,6 +4,7 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
