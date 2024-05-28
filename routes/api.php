5e36f0bcafffbe767390ce08c1c07364a672ea30
IkahnKakap <krf2308@gmail.com>
2024-05-28 13:29:45 +0700

    Co-authored-by: Muhammad Luthfi Khusyasy <Khusyasy@users.noreply.github.com>

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
